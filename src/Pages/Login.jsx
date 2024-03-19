f2ed97c2b147836e0fda25717f1e65d8cb4b93b1
Ashish Ranjan <88059924+codewithaashu@users.noreply.github.com>
2024-03-19 10:37:29 +0530

    login loading page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -13,6 +13,7 @@ const Login = () => {
     username: "",
     password: "",
   });
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const loginUser = async () => {
     try {
@@ -20,6 +21,7 @@ const Login = () => {
         `${process.env.REACT_APP_SERVER_BASE_URL}/api/login`,
         formData
       );
+
       return data;
     } catch (err) {
       const { data } = err.response;
@@ -32,7 +34,9 @@ const Login = () => {
       WarningToast(`${emptyField} is required field.`);
       return;
     }
+    setLoading(true);
     const resp = await loginUser();
+    setLoading(false);
     if (resp.success) {
       SuccessToast(resp.message ?? "Successfull");
       setFormData({
@@ -54,49 +58,55 @@ const Login = () => {
   };
   return (
     <div className="flex flex-row justify-center items-center h-screen">
-      <div className="bg-white w-11/12 md:w-1/4 px-7 py-10 rounded-md shadow-md flex flex-col gap-4">
-        <h1 className="text-2xl font-semibold text-black text-center mb-3">
-          Login
-        </h1>
-        <SelectComponent
-          label={"User Type"}
-          data={["Admin", "Super Admin"]}
-          defaultValue={"Select User Type"}
-          field={"userType"}
-          formData={formData}
-          setFormData={setFormData}
-        />
-        <div className="flex flex-col gap-1">
-          <h1 className="text-[15px] font-medium text-gray-600">Username</h1>
-          <input
-            type="text"
-            value={formData.username}
-            className={`file-input w-full  h-fit p-2 bg-white text-sm border-[1px] border-gray-800 text-gray-700 focus:outline-none placeholder:text-sm py-3`}
-            placeholder="Enter Username Here..."
-            onChange={(e) => {
-              setFormData({ ...formData, username: e.target.value });
-            }}
+      {!loading ? (
+        <div className="bg-white w-11/12 md:w-1/4 px-7 py-10 rounded-md shadow-md flex flex-col gap-4">
+          <h1 className="text-2xl font-semibold text-black text-center mb-3">
+            Login
+          </h1>
+          <SelectComponent
+            label={"User Type"}
+            data={["Admin", "Super Admin"]}
+            defaultValue={"Select User Type"}
+            field={"userType"}
+            formData={formData}
+            setFormData={setFormData}
           />
+          <div className="flex flex-col gap-1">
+            <h1 className="text-[15px] font-medium text-gray-600">Username</h1>
+            <input
+              type="text"
+              value={formData.username}
+              className={`file-input w-full  h-fit p-2 bg-white text-sm border-[1px] border-gray-800 text-gray-700 focus:outline-none placeholder:text-sm py-3`}
+              placeholder="Enter Username Here..."
+              onChange={(e) => {
+                setFormData({ ...formData, username: e.target.value });
+              }}
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <h1 className="text-[15px] font-medium text-gray-600">Password</h1>
+            <input
+              type="password"
+              value={formData.password}
+              className={`file-input w-full  h-fit p-2 bg-white text-sm border-[1px] border-gray-800 text-gray-700 focus:outline-none placeholder:text-sm py-3`}
+              placeholder="Enter Password Here..."
+              onChange={(e) => {
+                setFormData({ ...formData, password: e.target.value });
+              }}
+            />
+          </div>
+          <button
+            className="bg-blue py-2 text-white text-lg font-bold mt-5"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
         </div>
-        <div className="flex flex-col gap-1">
-          <h1 className="text-[15px] font-medium text-gray-600">Password</h1>
-          <input
-            type="password"
-            value={formData.password}
-            className={`file-input w-full  h-fit p-2 bg-white text-sm border-[1px] border-gray-800 text-gray-700 focus:outline-none placeholder:text-sm py-3`}
-            placeholder="Enter Password Here..."
-            onChange={(e) => {
-              setFormData({ ...formData, password: e.target.value });
-            }}
-          />
+      ) : (
+        <div className="self-center py-10">
+          <span className="loading loading-spinner loading-lg bg-blue"></span>
         </div>
-        <button
-          className="bg-blue py-2 text-white text-lg font-bold mt-5"
-          onClick={handleLogin}
-        >
-          Login
-        </button>
-      </div>
+      )}
     </div>
   );
 };
